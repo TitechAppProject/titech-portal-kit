7d8d17bb19461e7cdfc122167ab24541adb60d1a
nanashiki <nanashiki.app@gmail.com>
2021-11-27 12:45:28 +0900

    Update support platforms

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let package = Package(
     name: "TitechPortalKit",
     platforms: [
         .macOS(.v12),
+        .iOS(.v15),
+        .watchOS(.v8),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
