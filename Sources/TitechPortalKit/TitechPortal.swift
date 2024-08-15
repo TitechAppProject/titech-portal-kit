67fee27f0efbb37ac1d52f1b2d1e6ea5033e1b7a
nanashiki <nanashiki.app@gmail.com>
2024-08-15 23:39:10 +0900

    Update User Agent

--- Sources/TitechPortalKit/TitechPortal.swift
+++ Sources/TitechPortalKit/TitechPortal.swift
@@ -18,7 +18,7 @@ public enum TitechPortalLoginError: Error, Equatable {
 
 public struct TitechPortal {
     private let httpClient: HTTPClient
-    public static let defaultUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1"
+    public static let defaultUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1"
 
     public init(urlSession: URLSession, userAgent: String = TitechPortal.defaultUserAgent) {
         self.httpClient = HTTPClientImpl(urlSession: urlSession, userAgent: userAgent)
